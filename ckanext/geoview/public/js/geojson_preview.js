56c027d5b71ff0a442e5f8fc309e0e142ac7859c
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-01-20 11:22:38 +0100

    Add projection definition for code EPSG:3044

--- ckanext/geoview/public/js/geojson_preview.js
+++ ckanext/geoview/public/js/geojson_preview.js
@@ -37,6 +37,7 @@ ckan.module('geojsonpreview', function (jQuery, _) {
       // urn:ogc:def:crs:OGC::CRS84, but proj4s uses a different name
       // for it. See https://github.com/ckan/ckanext-geoview/issues/51
       proj4.defs['OGC:CRS84'] = proj4.defs['EPSG:4326'];
+      proj4.defs('EPSG:3044', '+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs');
     },
 
     showError: function (jqXHR, textStatus, errorThrown) {
